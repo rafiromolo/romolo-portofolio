26baa6f026ea0ba4ed82eece152c96eb31fd5ce1
rafiromolo <rafiromolo@gmail.com>
2026-09-12 21:52:01 +0700

    add config for vercel url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portofolio-rafi-romolo.vercel.app',
   integrations: [],
   adapter: vercel()
 });
